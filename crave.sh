0c2cbc8f426bde204474c8ca542420783932cb66
droidcore <agusmahardika99@gmail.com>
2025-12-30 05:44:04 +0700

    Add files via upload

---
+++ crave.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+# ================================
+# Project Configuration
+# ================================
+export PROJECTFOLDER="LOS"
+export PROJECTID="93"
+export REPO_INIT="https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs --depth=1"
+# ================================
+# Destroy Old Clones
+# ================================
+echo ">>> Cleaning old clone"
+if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) || [ "${DCDEVSPACE}" == "1" ]; then
+  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else
+  rm -rf $PROJECTFOLDER || true
+fi
+
+# ================================
+# Create New Clone
+# ================================
+echo ">>> Creating new clone"
+if [ "${DCDEVSPACE}" == "1" ]; then
+  crave clone create --projectID $PROJECTID /crave-devspaces/$PROJECTFOLDER || echo "Crave clone create failed!"
+  cd /crave-devspaces/$PROJECTFOLDER
+else
+  mkdir $PROJECTFOLDER && cd $PROJECTFOLDER
+  echo "Running $REPO_INIT"
+  $REPO_INIT
+fi
+
+# ================================
+# Run inside Crave devspace
+# ================================
+crave run --no-patch -- "
+  # ================================
+  # Clean old manifests
+  # ================================
+  rm -rf .repo/local_manifests
+  rm -rf device/xiaomi/peridot
+  rm -rf out/target/product/peridot
+  
+  # ================================
+  # Initialize Evox repo
+  # ================================
+  echo '>>> Initializing Lunaris-AOSP repo'
+  $BUILD_DIFFERENT_ROM
+
+  # ================================
+  # Clone local manifests
+  # ================================
+  echo '>>> Cloning local manifests'
+  git clone https://github.com/droidcore/manifest_peridot.git -b lineage-23.0 .repo/local_manifests/
+
+  # ================================
+  # Sync sources
+  # ================================
+  echo '>>> Syncing sources'
+  /opt/crave/resync.sh
+
+  # ================================
+  # Setup build environment
+  # ================================
+  . build/envsetup.sh
+  export BUILD_USERNAME=BLU
+  export BUILD_HOSTNAME=crave
+  export TZ=Asia/Jakarta
+
+  # ================================
+  # Build
+  # ================================
+  echo '>>> Starting build'
+  lunch lineage_peridot-bp3a-user
+  make installclean
+  m evolution 
+"
